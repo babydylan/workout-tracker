15287c7e45ccd3aca083e382469828ff6caf8203
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-29 23:35:23 -0700

    added route to update workouts

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const db = require("../models/workout");
 
-router.post("/api/workouts", (req, res) => {
+router.post("/api/exercise", (req, res) => {
   db.Workout.create(req.body)
   .then((data) => {
     console.log(data);
@@ -12,12 +12,35 @@ router.post("/api/workouts", (req, res) => {
   });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.update(
+    {
+      _id: (req.params.id),
+    },
+    {
+      $push: {
+       exercises: req.body
+      },
+      $inc:{
+        totalDuration:req.body.duration
+      },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  .then((data) => {
+    res.json(data)
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
