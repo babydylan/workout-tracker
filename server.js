2f56b5feb0ecf914599b82ad5407a918cdda7bd3
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-29 20:48:03 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // app.use(require("./routes/api.js"));
 
